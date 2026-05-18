4db9100c6e64f40a0f6b96e7d89d610f89fd85b5
Vinc551 <vwu6022@gmail.com>
2026-05-18 14:19:25 +0100

    addednsome code-trying to test around for the first day

--- Speech-Recognition test.py
+++ Speech-Recognition test.py
@@ -1,1 +1,6 @@
 # Speech Recognition test 1
+import speech_recognition as sr
+# Initialize recognizer
+r = sr.Recognizer()
+# Use the microphone as source for input.
+with sr.Microphone() as source:
