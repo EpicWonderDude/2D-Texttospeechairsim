145c1e1ca81a063f9b533339dbfb872e27ec6a0e
Vinc551 <vwu6022@gmail.com>
2026-05-18 14:48:24 +0100

    updated file

--- Speech-Recognition test.py
+++ Speech-Recognition test.py
@@ -4,3 +4,10 @@ import speech_recognition as sr
 r = sr.Recognizer()
 # Use the microphone as source for input.
 with sr.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize your voice")
