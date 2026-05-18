c19f44671d57fa4a55f9985157d57a6c23d27747
EpicWonderDude <zacmcdlt@gmail.com>
2026-05-18 14:23:07 +0100

    New function for passing string

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+string = "Turn right 40 degrees"
+splitspring = string.split()
+print(splitspring[2])
+print(splitspring[1])
+
+
+
+
+degrees = splitspring[1]
+direction = splitspring[2]
+
+print("The direction is", degrees, "the degrees are:", direction)
